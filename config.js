c27c6f8069e5f2b151cd3bf40b427ad66645f213
Saalim Khan <68114437+saalimkhan13@users.noreply.github.com>
2020-10-28 01:59:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,13 @@ import firebase from 'firebase';
 require('@firebase/firestore')
 
 var firebaseConfig = {
-    apiKey: "AIzaSyASyOjOtJla-X-b3io2eLoaUc_bIRFSIIc",
-    authDomain: "book-santa-app.firebaseapp.com",
-    databaseURL: "https://book-santa-app.firebaseio.com",
-    projectId: "book-santa-app",
-    storageBucket: "book-santa-app.appspot.com",
-    messagingSenderId: "69634746716",
-    appId: "1:69634746716:web:6fbbfc110fb4475365f999",
-    measurementId: "G-DLB7XC0JPL"
+   apiKey: "AIzaSyAR5kgOjSXcHZinjDClBN34rEPT0FLfH4w",
+  authDomain: "project34-3ea01.firebaseapp.com",
+  databaseURL: "https://project34-3ea01.firebaseio.com",
+  projectId: "project34-3ea01",
+  storageBucket: "project34-3ea01.appspot.com",
+  messagingSenderId: "1094664179155",
+  appId: "1:1094664179155:web:c22f4b6231ff51c89cb326"
   };
 
 
